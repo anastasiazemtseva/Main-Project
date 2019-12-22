5caacd5f414498281bdd1cc1ea0e90fa31d9794e
Настя <a.zemtseva@g.nsu.ru>
2019-12-22 21:59:21 +0700

    Files is done.

--- main project.py
+++ main project.py
@@ -1,28 +1,53 @@
-from questions import quests, answ
 import timeit
 import random
 
+name = input('Enter your name:\n')
+subject = input('Select subject (Mathematical analysis, Microeconomics, Discrete math)\n')
+
+if subject == 'Mathematical analysis':
+    from Mathematical_analysis import quests, answ
+elif subject == 'Microeconomics':
+    from Microeconomics import quests, answ
+elif subject == 'Discrete math':
+    from Discrete_Math import quests, answ
+
 timer = timeit.default_timer()
 
-counter = 1
+number = 1
+counter = 0
 
-while counter < 10:
+while number < 5:
     print()
     query = random.choice(quests)
-    print(counter, '. ', query, sep='')
+    print(number, '. ', query, sep='')
     answer = input().lower()
 
     if answer in answ:
         if query == answ[answer]:
             print('Правильно!')
+            number += 1
             counter += 1
         else:
             print('Неверно!')
-            counter += 1
+            number += 1
     else:
-        counter += 1
+        number += 1
         print('Ответ некорректный!')
 
 print()
+with open('input.txt', 'w') as in_file:
+    in_file.write(name)
+    in_file.write('\n')
+    in_file.write("-" * 53)
+    in_file.write('\n')
 
-print(timeit.default_timer() - timer)
+    test_time = 'Test time:'
+    points = 'Points earned:'
+    timer_t = str(round(timeit.default_timer() - timer)) + ' seconds'
+    points_t = str(counter) + ' points'
+
+    in_file.write("|{:^25s}|{:^25s}|".format(test_time, points))
+    in_file.write('\n')
+    in_file.write("|{:^25s}|{:^25s}|".format(timer_t, points_t))
+    in_file.write('\n')
+    in_file.write("-" * 53)
